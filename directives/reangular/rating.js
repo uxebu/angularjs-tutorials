5022b6a99f35fc6fce9152ea03b7e0f7b4bcbf1a
petehunt <floydophone@gmail.com>
2013-06-12 17:25:31 -0700

    port to react

--- directives/reangular/rating.js
+++ directives/reangular/rating.js
@@ -1,46 +1,45 @@
-angular.module('FundooDirectiveTutorial', [])
-  .controller('FundooCtrl', function($scope, $window) {
-    $scope.rating = 5;
-    $scope.saveRatingToServer = function(rating) {
-      $window.alert('Rating selected - ' + rating);
-    };
-  })
-  .directive('fundooRating', function () {
-    return {
-      restrict: 'A',
-      template: '<ul class="rating">' +
-                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-                    '\u2605' +
-                  '</li>' +
-                '</ul>',
-      scope: {
-        ratingValue: '=',
-        max: '=',
-        readonly: '@',
-        onRatingSelected: '&'
-      },
-      link: function (scope, elem, attrs) {
+/** @jsx React.DOM */
 
-        var updateStars = function() {
-          scope.stars = [];
-          for (var  i = 0; i < scope.max; i++) {
-            scope.stars.push({filled: i < scope.ratingValue});
-          }
-        };
-
-        scope.toggle = function(index) {
-          if (scope.readonly && scope.readonly === 'true') {
-            return;
-          }
-          scope.ratingValue = index + 1;
-          scope.onRatingSelected({rating: index + 1});
-        };
-
-        scope.$watch('ratingValue', function(oldVal, newVal) {
-          if (newVal) {
-            updateStars();
-          }
-        });
-      }
+var FundooRating = React.createClass({
+  render: function() {
+    var items = [];
+    for (var i = 0; i < this.props.max; i++) {
+      var className = i <= this.props.value ? 'filled' : '';
+      var clickHandler = this.props.onRatingSelected ? this.props.onRatingSelected.bind(this.props, i) : null;
+      items.push(
+        <li class={className} onClick={clickHandler}>
+          {'\u2605'}
+        </li>
+      );
     }
-  });
+    return (
+      <ul class="rating">
+        {items}
+      </ul>
+    );
+  }
+});
+
+var FundooDirectiveTutorial = React.createClass({
+  getInitialState: function() {
+    return {rating: 5};
+  },
+  handleRatingSelected: React.autoBind(function(rating) {
+    this.setState({rating: rating});
+    alert('Rating selected - ' + rating);
+  }),
+  render: function() {
+    return (
+      <div>
+        Rating is {this.state.rating}<br/>
+        Clickable Rating <br/>
+        <FundooRating value={this.state.rating} max="10" onRatingSelected={this.handleRatingSelected} />
+        <br />
+        Readonly rating <br/>
+        <FundooRating value={this.state.rating} max="10" />
+      </div>
+    );
+  }
+});
+
+React.renderComponent(<FundooDirectiveTutorial />, document.body);
